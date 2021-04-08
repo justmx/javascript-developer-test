a15193ad31d9246969b324078bde6da7fb432129
Matthew Ma <justmxk@gmail.com>
2021-04-08 22:45:53 +1000

    implement getArnieQuotes function

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,25 @@
-const { httpGet } = require('./mock-http-interface');
+const { httpGet } = require("./mock-http-interface");
+
+const getHttpResult = async (url) => {
+  const res = await httpGet(url);
+
+  // Optional chaining in higher version node
+  // const message = JSON.parse(res?.body ?? "{}").message;
+  const message = JSON.parse((res && res.body) || "{}").message;
+
+  const result =
+    res.status === 200
+      ? { "Arnie Quote": message }
+      : {
+          FAILURE: message,
+        };
+  return Promise.resolve(result);
+};
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  if (!urls || urls.length === 0) return [];
+  const results = await Promise.all(urls.map(getHttpResult));
+  return results;
 };
 
 module.exports = {
